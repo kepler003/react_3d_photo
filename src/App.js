c596c5b5a33ba8c681b83c5cb7ffcd9773cb8459
Bartłomiej Pyzocha <b.pyzocha@gmail.com>
2020-05-23 23:25:41 +0200

    Remove image

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './styles/styles.css';
 
 const calc = (x, y) => [(y - window.innerHeight / 2) / 8, -(x - window.innerWidth / 2) / 8, 1.1];
 const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
-const shadow = (x, y, s) => `0 0 ${3 + (200 * (s - 1))}px ${30 * (1 - s)}px hsl(0, 0%, 65%)`;
+const shadow = (x, y, s) => `0 ${1 + (100 * (s - 1))}px ${3 + (200 * (s - 1))}px ${1 + (30 * (1 - s))}px hsl(0, 0%, 65%)`;
+const color = (x, y, s) => `hsl(220, 50%, ${50 + (x / 1.5)}%)`;
 
 function App() {
   
@@ -16,14 +17,11 @@ function App() {
       className={'card'}
       style={{ 
         boxShadow: props.xys.interpolate(shadow),
-        transform: props.xys.interpolate(trans) 
+        transform: props.xys.interpolate(trans),
+        backgroundColor: props.xys.interpolate(color)
       }}
       onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
-      onMouseLeave={() => set({xys: [0, 0, 1]})}>
-        <div className={'sub-card'}>
-          <img src={'travel.jpg'} className={'image'} />
-        </div>
-      </animated.div>
+      onMouseLeave={() => set({xys: [0, 0, 1]})} />
     </div>
   );
 }
